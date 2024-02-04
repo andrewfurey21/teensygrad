403c03c828650e2226cee74857e1b3fd877389e5
andrewfurey21 <andrewfurey2003@gmail.com>
2024-02-04 17:23:47 +0000

    starting work on backprop and optimizers

---
+++ teensygrad/graph.c
@@ -0,0 +1,11 @@
+#include "teensygrad.h"
+
+//calculate gradient of current,
+//TODO: accumulate gradients and add zeroing out function instead
+void backwards(struct tensor* current, struct tensor* current_grads) {
+
+
+}
+
+void add_backwards(struct tensor* current, struct tensor* current_grads) {
+}
