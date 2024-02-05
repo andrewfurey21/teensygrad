28f61243ec479c966686b4b5ed52dc3816b6362d
andrewfurey21 <andrewfurey2003@gmail.com>
2024-02-05 15:35:03 +0000

    added max dims, return pointer instead of value in create shape functions, moved same_shape to here and added print function

--- teensygrad/shape.c
+++ teensygrad/shape.c
@@ -1,26 +1,29 @@
 #include "stdint.h"
+#include "assert.h"
 #include "stdlib.h"
 #include "stdio.h"
 #include "teensygrad.h"
 
-struct shape create_shape(uint32_t* dims, uint32_t size) {
+#define MAX_DIMS 4
+
+struct shape* create_shape(uint32_t* dims, uint32_t size) {
     uint32_t* dims_copy = (uint32_t*)malloc(size * sizeof(uint32_t));
     for (uint32_t i = 0; i < size; i++) {
         dims_copy[i] = dims[i];
     }
-    struct shape ret;
-    ret.dims = dims_copy;
-    ret.size = size;
+    struct shape* ret = (struct shape*)malloc(sizeof(struct shape));
+    ret->dims = dims_copy;
+    ret->size = size;
     return ret;
 }
 
-struct shape create_shape_1d(uint32_t dim) {
+struct shape* create_shape_1d(uint32_t dim) {
     uint32_t* dims = (uint32_t*)malloc(sizeof(uint32_t));
     dims[0] = dim;
     return create_shape(dims, 1);
 }
 
-struct shape create_shape_2d(uint32_t dim1, uint32_t dim2) {
+struct shape* create_shape_2d(uint32_t dim1, uint32_t dim2) {
     uint32_t* dims = (uint32_t*)malloc(2*sizeof(uint32_t));
     dims[0] = dim1;
     dims[1] = dim2;
@@ -29,4 +32,25 @@ struct shape create_shape_2d(uint32_t dim1, uint32_t dim2) {
 
 void free_shape(struct shape* s) {
     free(s->dims);
+}
+
+bool same_shape(struct tensor* a, struct tensor* b) {
+    if (a->shape_b->size != b->shape_b->size) {
+        return false;
+    }
+    for (uint32_t i = 0; i < a->shape_b->size; i++) {
+        if (a->shape_b->dims[i] != b->shape_b->dims[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void print_shape(struct shape* s) {
+    assert(s->size <= MAX_DIMS && "Too many dimensions in shape.");
+    printf("shape:(");
+    for (size_t i = 0; s->dims[i] != 0 && i < MAX_DIMS; i++) {
+        printf("%d,", s->dims[i]);
+    }
+    printf(")\n");
 }
